e6fb9da3ec4229031a0ddb997e2a8c1f7455c710
Hari krishnan <143381736+HariOp6238@users.noreply.github.com>
2023-12-01 23:25:42 +0530

    splash logo added

--- lib/splashscreen/splash.dart
+++ lib/splashscreen/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:meteorpoint/view/homepage.dart';
 
 class Splash extends StatefulWidget {
@@ -11,7 +12,7 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 2)).then((value) =>
+    Future.delayed(const Duration(seconds: 3)).then((value) =>
         Navigator.pushReplacement(context,
             MaterialPageRoute(builder: (context) => const Homepage())));
     super.initState();
@@ -20,27 +21,20 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 239, 96, 52),
-              Color.fromARGB(255, 107, 49, 208)
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
+      body: Center(
+        child: Stack(
+          children: [
+            SizedBox(
+                child: Lottie.asset('assets/Animation - 1701448195937.json',
+                    fit: BoxFit.fill)),
+          ],
         ),
-        child: Center(
-            child: Image.asset(
-          'assets/0.png',
-          scale: 1,
-        )),
       ),
     );
   }
